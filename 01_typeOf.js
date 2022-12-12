8c52f06f18bdd21f24785dcaf89359311646c739
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-12 21:53:28 +0530

    typeof
    typeof

---
+++ 01_typeOf.js
@@ -0,0 +1,22 @@
+var bankName 
+bankName= " SBI Bank";
+console.log(bankName);
+console.log(typeof "Billgates"); var microsoftOwner ="Billgates";
+var percentage
+ percentage= "90%"
+ console.log(percentage);
+ console.log(typeof "Billgates"); var microsoftOwner ="Billgates";
+var name1
+name1 ="true"
+console.log(name1);
+console.log(typeof false); var areYouMad = false;
+var number
+number="Hundred and seven"
+console.log(number);
+console.log(typeof "Billgates"); var microsoftOwner ="Billgates";
+ var num
+ num= "107";
+ console.log(num);  
+var num
+num="number";
+console.log(num);
