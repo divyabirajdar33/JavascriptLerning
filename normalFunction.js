ef3f51cd6aa30d2af3d2f14006ebf6d0b6e531f5
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-12 21:56:06 +0530

    normal function
    normal function

---
+++ normalFunction.js
@@ -0,0 +1,12 @@
+function Details(my){
+    console.log(my);
+     } 
+Details("Bank Details:-");
+function bankDetails(bank){
+    console.log(bank);
+    }
+    bankDetails("Bank Name :-  CITI Bank, AccountNumber:3456782345, city:pune, pincode:413202");
+    bankDetails("Bank Name :-  Axis Bank, AccountNumber:7856782345, city:Mumbai, pincode:441202");   
+    bankDetails("Bank Name :-  HDFC Bank, AccountNumber:8956782345, city:pune, pincode:613202");
+
+    
