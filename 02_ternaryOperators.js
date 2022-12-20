0b04c3be4c9acb4171621337a88ae65616e046c8
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-20 19:08:39 +0530

    ternary
    ternary

---
+++ 02_ternaryOperators.js
@@ -0,0 +1,40 @@
+var num1 = 10;
+var num2 = -10;
+var result = num1 >= num2 ? "10" : "-10";
+console.log(`1) Greater Number among 10 and -10 is "${result}"`);
+
+var num1 = 800;
+var num2 = 899;
+var result = num1 >= num2 ? "800" : "899";
+console.log(`2) Greater Number among 800 and 899 is "${result}"`);
+
+var num = 29;
+var result = num % 2 == 0 ? "Even" : "Odd";
+console.log(`\n1) The number 29 is "${result}"`);
+
+var num = 44;
+var result = num % 2 == 0 ? "Even" : "Odd";
+console.log(`2) The number 44 is "${result}"`);
+
+var num = 0;
+var result = num % 2 == 0 ? "Even" : "Odd";
+console.log(`3) The number 0 is "${result}"`);
+
+var num = 101;
+var result = num % 2 == 0 ? "Even" : "Odd";
+console.log(`4) The number 101 is "${result}"`);
+
+var value = "JavaScript";
+var len = value.length;
+var result = len % 2 == 0 ? "Even" : "Odd";
+console.log(`\n1) The length of word JavaScript is "${result}"`);
+
+var value = "Developer";
+var len = value.length;
+var result = len % 2 == 0 ? "Even" : "Odd";
+console.log(`2) The length of word Developer is "${result}"`);
+
+var value = "Google";
+var len = value.length;
+var result = len % 2 == 0 ? "Even" : " Odd";
+console.log(`3) The length of word Google is "${result}"`);
