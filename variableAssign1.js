9fd79635a5ca39f58bbc0d75c7b966abe225f908
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-20 17:19:37 +0530

    variable
    variable

---
+++ variableAssign1.js
@@ -0,0 +1,19 @@
+var  myName; 
+myName = "Divya Birajdar"; 
+console.log(myName);
+
+myFullName ="Divya Santosh Birajdar";
+console.log(myFullName);
+
+var myLove;
+myLove = "I love JavaScript";
+console.log(myLove);
+
+var myName = "Divya Birajdar";
+var age ="21";
+var address = "uruli kanchan,Pune";
+var pin_code = "4122202";
+var gender = "Female";
+var is_married = "Yes";
+console.log(myName,age,address,pin_code,age,gender,is_married);
+
