7ffb0b803d682ab69890f9f6382f5352c75d2cd8
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-20 19:14:24 +0530

    for loop statement
    for loop statement

---
+++ forLoopAssign.js
@@ -0,0 +1,12 @@
+for (let index = 5; index <15; index+1) {
+   console.log(index);  
+}
+for (let index = 50; index <40; index-1) {
+   console.log(index);  
+}
+for (let index = 5; index <15; index+1) {
+   console.log(index);  
+}
+for (let index = 5; index <15; index+1) {
+   console.log(index);  
+}
