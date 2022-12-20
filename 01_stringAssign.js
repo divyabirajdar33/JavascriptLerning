d1aff51af0fd83ea7ca1b91ee4a6707a2697a686
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-20 18:59:33 +0530

    string
    string

---
+++ 01_stringAssign.js
@@ -0,0 +1,14 @@
+var companyName;
+companyName = "TCS";
+console.log("My dream company:-",companyName);
+var hobby1;
+hobby1 ="Listening Music";
+var hobby2;
+hobby2 = "Travelling";
+var hobby3;
+hobby3 = "Learning new things";
+String.endwith
+console.log("\n My Hobbies are:-",hobby1,hobby2,hobby3);
+console.log(`\n My Hobbies are:-${hobby1}${hobby2}${hobby3}`);
+
+
