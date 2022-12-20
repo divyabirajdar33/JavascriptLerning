586bfd5f83df6ded9d31288fd27e42d10513a595
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-20 19:09:37 +0530

    compare
    compare

---
+++ 01_compareAssign.js
@@ -0,0 +1,32 @@
+
+var num1 = "0";
+var num2 =" ' '";
+console.log(`\n 1) 0 == ' ' ---> ${0 =="0"}`);
+
+var num1 = "0";
+var num2 =" ' '";
+console.log(`\n 2) 0 == '0' ---> ${0 =="0"}`);
+
+var num1 = "0";
+var num2 =" ' '";
+console.log(`\n 3) 0 == false  ---> ${0 =="0"}`);
+
+var num1 = "0";
+var num2 =" ' '";
+console.log(`\n 4) null == underfined ---> ${0 =="0"}`);
+
+var num1 = "0";
+var num2 =" ' '";
+console.log(`\n 5) 1 == [1] ---> ${0 =="0"}`);
+
+var num1 = "0";
+var num2 =" ' '";
+console.log(`\n 6) 1 == true  ---> ${0 =="0"}`);
+
+var num1 = "0";
+var num2 =" ' '";
+console.log(`\n 7) 1 == '1' ---> ${0 =="0"}`);
+
+var num1 = "0";
+var num2 =" ' '";
+console.log(`\n 8) 0 '' ==  ${0 =="0"}`);
