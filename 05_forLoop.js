fa6b1f0bd204857099934976e9154dcae64853e8
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-21 21:22:16 +0530

    for loop
    for loop

---
+++ 05_forLoop.js
@@ -0,0 +1,31 @@
+ for (let index = 5; index <= 15; index+1) {
+  console.log(`${index}`);
+   
+ }
+ for (let index = 50; index>=40; index--) {
+   console.log(`${index}`);
+}
+console.log(`----------`);
+for (let index = 1; index<=30; index++) {
+   if(index %2 == 1){
+   console.log(`${index}`);
+}
+}
+console.log(`-----------`);
+for (let index = 1; index<=20; index++){
+if (index %2 == 0) {
+   console.log(`${index}`);
+}
+}
+console.log(`------------`);
+for (let index = 5; index<=50; index+5) {
+   console.log(`${index}`);
+}
+console.log(`--------------`);
+for (let index = 10; index<=100; index+10) {
+   console.log(`${index}`);
+}
+console.log(`------------`);
+for (let index = 100; index>=10; index-10) {
+   console.log(`${index}`);
+}
