9dece31cd438b83c8135aa4b85583d94521b5387
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-21 21:23:07 +0530

    while and do while loop
    while and do while loop

---
+++ whileAndDoWhileAssign.js
@@ -0,0 +1,45 @@
+var index = 5;
+while (index<=15) {
+   console.log(`${index}`);
+   index+1;
+}
+var index = 50;
+while (index>=40) {
+   console.log(`${index}`);
+   index-1;
+}
+var index = 1;
+while (index<=30) {
+   if (index %2 == 1) 
+   {
+   console.log(`${index}`);
+      index+1;
+   }
+}
+
+var index = 1;
+while (index<=20) 
+if (index %2 == 0) {
+   {
+      console.log(`${index}`);
+      index+1;  
+}
+}
+
+var index = 5;
+do {
+   console.log(index);
+   index+5;
+} while (index<=50);
+
+var index = 10;
+do {
+   console.log(index);
+   index+10;
+} while (index<=100);
+
+var index = 100;
+do {
+   console.log(index);
+   index-10;
+} while (index>=10);
