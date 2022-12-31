92cc0d4cf6d2e79610489ac2c2e0328c0e2ae055
divyabirajdar33 <divyabirajdar33@gmail.com>
2022-12-31 06:59:38 +0530

    object
    object

---
+++ 02_object_assign.js
@@ -0,0 +1,39 @@
+console.log(`1) SBI Bank details:-`);
+const bank1 = {
+  bankName: "State Bank Of India",
+  location: "Uruli Kanchan",
+  accountNo: "455643789800",
+  IFSC: "SBI00000675",
+  intrestRate: "8%",
+};
+console.log(bank1);
+console.log(`\n`);
+console.log(`2) AXIS Bank details:-`);
+const bank2 = {
+  bankName: "AXIS Bank",
+  location: "swarget",
+  accountNo: "675698763408",
+  IFSC: "AXB00000782",
+  intrestRate: "10%",
+};
+console.log(bank2);
+console.log(`\n`);
+console.log(`3) HDFC Bank details:-`);
+const bank3 = {
+  bankName: "HDFC Bank",
+  location: "Hadpsar",
+  accountNo: "785647609122",
+  IFSC: "HDFC0000243",
+  intrestRate: "11%",
+};
+console.log(bank3);
+console.log(`\n`);
+console.log(`4) YES Bank details:-`);
+const bank4 = {
+  bankName: "YES Bank",
+  location: "pune",
+  accountNo: "670987784345",
+  IFSC: "YB00000908",
+  intrestRate: "14%",
+};
+console.log(bank4);
