d37196895f5eef1511869ab62c1081029086b467
divyabirajdar33 <divyabirajdar33@gmail.com>
2023-01-10 07:57:42 +0530

    typeof
    typeof

---
+++ typeOfAssign.js
@@ -0,0 +1,22 @@
+var bankName
+bankName = "SBI Bank";
+console.log(bankName);
+console.log(typeof "SBI Bank"); var bankName = "SBI Bank";
+var percentage
+percentage = "90%";
+console.log(percentage);
+console.log(typeof "90%"); var percentage = "90%";
+var name1
+name1= "true";
+console.log(name1);
+console.log(typeof true); var name1= true;
+var number
+number = "Hundred and seven";
+console.log(number);
+console.log(typeof "Hundred and seven"); var number = "Hundred and seven";
+var num
+num = "107";
+console.log(num);
+var num
+num="number";
+console.log(num);
